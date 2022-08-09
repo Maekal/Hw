3ad3611d2216d272a0a85cff5b83f628d9ffdc0c
Михаил <maekal0692@yandex.ru>
2022-08-09 21:24:04 +0300

    Сделано дз

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,36 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //задание 1
+
+        int i = 0;
+        while (i < 10) {
+            i++;
+            System.out.print(i + "");
+        }
+
+        System.out.println();
+
+        for (int a = 10; a > 0; a--) {
+            System.out.print(+a);
+        }
+
+
+        //Задание 2
+        {
+            System.out.print("\n");
+            int friday = 1;
+            for (int b = 1; b < 31; b = b + 7) {
+                System.out.println("Сегодня пятница"  + b +  "число.Необходимо подготовить отчет.");
+            }
+
+
+            //Задание 3
+            System.out.print("\n");
+            {
+                int start = 1896;
+                for (int year = 1896; year <= 2054; year = year + 79) {
+                    System.out.println(year);}
+            }
+        }
     }
 }
